f7834a556220698cec028ef5f15284445416d89d
이승찬 <schlee85@naver.com>
2024-02-12 21:08:33 +0900

    수정

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -34,28 +34,29 @@ export default function About() {
 						이승찬 <sub>(L.Seungchan, 경력 10년+)</sub>
 					</strong>
 					<p className="txt">
-						저는 UI 개발자입니다.
+						저는 UI 개발자입니다. 흔히 웹 퍼블리셔라 불립니다.
 						<br />
-						통상적으로 웹 퍼블리셔이고, 개발자로서의 <br className="mobile" />
-						마인드를 확장해 나가고 있으며, <br className="mobile" />
+						개발자로서의 마인드를 확장해 나가고 있으며,{' '}
+						<br className="mobile" />
 						보다 나은 경험을 드리기 위해 노력합니다.
 					</p>
 					<p className="txt">
-						웹 퍼블리싱은 사용자 중심의 웹사이트 제작에{' '}
+						웹 퍼블리싱은 사용자 중심의 웹사이트 제작에 초점을{' '}
 						<br className="mobile" />
-						초점을 맞추는 것입니다. <br className="pc" />
-						이를 위해, 사용자 경험과 <br className="mobile" />
-						인터페이스에 대한 깊은 이해를 바탕으로, <br className="mobile" />
-						사용자 친화적인 웹사이트를 개발해야 합니다.
-						<br />
-						뿐만 아니라, 웹 표준과 웹 접근성을 준수하여,{' '}
+						맞추는 것입니다. <br className="pc" />
+						이를 위해, 사용자 경험과 인터페이스에 <br className="mobile" />
+						대한 깊은 이해를 바탕으로, 사용자 친화적인 웹사이트를{' '}
 						<br className="mobile" />
-						모든 사용자가 웹사이트를 편리하게 <br className="mobile" />
+						개발해야 합니다. <br className="pc" />
+						뿐만 아니라, 웹 표준과 웹 접근성을 <br className="mobile" />
+						준수하여, 모든 사용자가 웹사이트를 편리하게{' '}
+						<br className="mobile" />
 						사용할 수 있도록 합니다.
 					</p>
 					<p className="txt">
-						저는 디자인을 시작으로 퍼블리싱까지 <br className="mobile" />
-						많은 것을 배워 왔습니다. <br className="pc" />
+						저는 디자인을 시작으로 퍼블리싱까지 많은 것을{' '}
+						<br className="mobile" />
+						배워 왔습니다. <br className="pc" />
 						그럼에도, UI 공부를 하다보면 <br className="mobile" />
 						매번 새롭고 끝이 없다는 것을 느낍니다.
 						<br />
@@ -71,9 +72,13 @@ export default function About() {
 					</p>
 					<strong className="tit">개인용 가이드</strong>
 					<p className="txt">
-						&middot; 가이드 : <br className="mobile" />
-						<Link to="#" target="_blank" title="새창열기">
-							test
+						&middot; 저장소 : <br className="mobile" />
+						<Link
+							to="https://github.com/schlee85/guide"
+							target="_blank"
+							title="새창열기"
+						>
+							https://github.com/schlee85/guide
 						</Link>
 					</p>
 					<strong className="tit">
